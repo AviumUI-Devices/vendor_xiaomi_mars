ebea20dcd6d0b713434e9a0a2f904553cde0779b
WilliamSado <williamsado1004@gmail.com>
2026-02-14 20:01:30 +0800

    mars: Extract blobs from miui_STAR_OS2.0.1.0.UKACNXM

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),mars)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,19a4190e96ca52b80da2d9e5489606da72095b81)
+$(call add-radio-file-sha1-checked,radio/aop.img,140f0f397254bd96e25f387d4635261582133505)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,18c1bf82d1920b5163b15d78dee7f79c5f9cc55a)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,44e7ffbe38661d2c57de7d90885e34cf33eef367)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,4b26d05015df54cde00b663509fe42617c508582)
+$(call add-radio-file-sha1-checked,radio/dsp.img,453413b40e59bfbd20bcb03dce4e3538f0f04550)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,a2b97ea28819fbb44b3c42d4345fff8699ddc73a)
+$(call add-radio-file-sha1-checked,radio/hyp.img,b88badf82565efba3ce6c4cde500e15365e534a3)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,9a69a53c8008ab8254a43d9130003809a8c976d4)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,59df627c6b6308fa180d1f307e9fa4bd3cfe416b)
+$(call add-radio-file-sha1-checked,radio/modem.img,9bc5685972bd575e234ceecda55729233ac08708)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,6b3b5ca9b1c1dc957158fe092e650c5318566c6a)
+$(call add-radio-file-sha1-checked,radio/shrm.img,56023294d657fc3e83fc5ca5f5819c90e95f6a3d)
+$(call add-radio-file-sha1-checked,radio/tz.img,4d44a8e41adaf0559800155cc974267372f808c8)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,1c925777c1ea0515e1a07fe99d4179c4abbad5de)
+$(call add-radio-file-sha1-checked,radio/xbl.img,dd5f24aa8fe0b4403171a99788daca6cb65794ef)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,ac0bfb4e425425e04a961394bd8a700b500b2cf6)
+
+endif
