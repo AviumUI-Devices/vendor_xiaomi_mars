18b892d22e71c971d67bdc57af1a2021be5e3ad0
WilliamSado <williamsado1004@gmail.com>
2026-03-29 18:57:48 +0800

    mars: Stop include firmware
    Change-Id: Ib9230ca2e841eefb76ea69f07650f27e47823f5e

--- Android.mk
+++ Android.mk
@@ -6,22 +6,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),mars)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,576ca350e30dd5d23910c5d712cae9f847b2c4f8)
-$(call add-radio-file-sha1-checked,radio/aop.img,ace2aea52c7a1653b4e971a888940d565346cc87)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,a4d27e860910d06e3f1650a46b6f4cd3f776d752)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,86a6c9134169b81f71322f3f78d18cacb2496311)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,32d62d4516aaae1d5c5d3ba6aaa06f5bd9acb0b5)
-$(call add-radio-file-sha1-checked,radio/dsp.img,453413b40e59bfbd20bcb03dce4e3538f0f04550)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,98dcf0ca2fc1811a6bdd0221b4052a9886d1a373)
-$(call add-radio-file-sha1-checked,radio/hyp.img,1bc3459c759708c80653d8542b35c19ca765847b)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,df41ac09b7eb0b84019fd06c934e062dc6e72777)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,dad67b871b5e7527d642aa42f5cec58976aaa3af)
-$(call add-radio-file-sha1-checked,radio/modem.img,108c8c895e22639ff5a3b03d9ccdbbd10ec7a2d7)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,6609f56a34bb4d75724e95dae3b1edb2986c0005)
-$(call add-radio-file-sha1-checked,radio/shrm.img,ceaf35cf9feb34be453d15a898ec1f176ccfed09)
-$(call add-radio-file-sha1-checked,radio/tz.img,1780ab2deb3c81c86696593803918d3e11287bb0)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,bb1d4db7932ae8cd4b4c3a3e6c3e5e4dbb3a69d6)
-$(call add-radio-file-sha1-checked,radio/xbl.img,ed70be5a3636ca3bc0383fc5f4f37775fecdbd61)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,fdeda7f258b825f87154a15c425eea022404e130)
-
 endif
